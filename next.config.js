0d02dc4102f96d9ea50e4c755d7b568684c28739
szl <chocolly@163.com>
2025-08-08 23:26:29 +0800

    test: auth

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.well-known\/webauthn$/,
+            type: "asset/resource",
+            generator: {
+                filename: ".well-known/webauthn"
+            }
+        });
+        return config;
+    }
+};
 
 module.exports = nextConfig;
